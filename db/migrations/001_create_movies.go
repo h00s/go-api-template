fd926bdf00653e395880e8986ad6ed682d2c02cb
Krunoslav Husak <kruno@husak.me>
2023-01-31 23:38:31 +0100

    Added unique index on movie's title and release year

--- db/migrations/001_create_movies.go
+++ db/migrations/001_create_movies.go
@@ -11,7 +11,9 @@ const CreateMovies = `
 		rating FLOAT NOT NULL,
 		created_at TIMESTAMP NOT NULL DEFAULT NOW()
 	);
-	
+
+	CREATE UNIQUE INDEX movies_title_release_year_idx ON Movies (title, release_year);
+
 	UPDATE schema_migrations SET version = 1 WHERE version = 0;
 	
 	COMMIT;
